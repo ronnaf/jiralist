e7a917898aeeec0ed66c9067d953e860bceaf5e7
Ronna Firmo <ronnwiseley@gmail.com>
2021-05-17 01:58:31 +0800

    feat: update home screen layout

--- src/modules/home/components/HomeScreen.tsx
+++ src/modules/home/components/HomeScreen.tsx
@@ -14,80 +14,91 @@ export const HomeScreen = (props: HomeProps) => {
   return (
     <Container>
       <InnerContainer>
+        {/* Header */}
         <JHeader displayName={props.currentUser?.displayName || '-'} />
         <Main>
+          {/* Sidebar */}
           <JSidebar
             projects={props.projects}
             loading={props.loadingProjects}
             handleLogout={props.userClickedLogout}
             handleProjectChange={props.userChangedCurrentProject}
           />
+          {/* Body */}
           <Body>
-            {props.bannerShown && (
-              <React.Fragment>
-                <JBanner
-                  title="Getting a 'See /corsdemo for more info' error?"
-                  message={
-                    <span>
-                      Go to <a href="https://cors-anywhere.herokuapp.com/">https://cors-anywhere.herokuapp.com/</a> and
-                      click `<b>Request temporary access to the demo server</b>`
-                    </span>
-                  }
-                  onClose={props.userToggledBanner}
-                />
-                <SizedBox height={16} />
-              </React.Fragment>
-            )}
+            {/* Current project name */}
             {props.currentProject && (
               <Flex>
                 <H1>{props.currentProject?.name}</H1>
                 <Regular>({props.currentProject?.key})</Regular>
               </Flex>
             )}
-            <SizedBox height={16} />
-            <H1>Issues</H1>
-            <Subtitle>Your issues that are currently in TODO or IN PROGRESS</Subtitle>
-            <SizedBox height={10} />
-            {props.loadingIssues ? (
-              <Regular>Loading...</Regular>
-            ) : (
-              <Issues>
-                {props.issues.map(issue => (
-                  <JIssueItem
-                    issueKey={issue.key}
-                    summary={issue.fields.summary}
-                    onCopy={() => copyToClipboard(getIssueLink(issue.key))}
-                    onMark={() => {}}
-                  />
-                ))}
-              </Issues>
+            {/* Info banner */}
+            {props.bannerShown && (
+              <JBanner
+                title="Getting a 'See /corsdemo for more info' error?"
+                message={
+                  <span>
+                    Go to <a href="https://cors-anywhere.herokuapp.com/">https://cors-anywhere.herokuapp.com/</a> and
+                    click `<b>Request temporary access to the demo server</b>`
+                  </span>
+                }
+                onClose={props.userToggledBanner}
+              />
             )}
-            <SizedBox height={30} />
-            <H1>Completed Issues</H1>
-            <Subtitle>Your issues that have been completed, but not necessarily READY FOR QA</Subtitle>
-            <SizedBox height={10} />
-            {props.loadingCompletedIssues ? (
-              <Regular>Loading...</Regular>
-            ) : (
-              <IssueGroups>
-                {props.completedIssueGroups.map(issueGroup => (
-                  <div key={issueGroup.dateCompletedString}>
-                    <Regular weight="bold">{issueGroup.dateCompletedString}</Regular>
-                    <SizedBox height={4} />
-                    <Issues>
-                      {issueGroup.issues.map(issue => (
-                        <JIssueItem
-                          issueKey={issue.key}
-                          summary={issue.summary}
-                          onCopy={() => copyToClipboard(getIssueLink(issue.key))}
-                          onMark={() => {}}
-                        />
-                      ))}
-                    </Issues>
-                  </div>
-                ))}
-              </IssueGroups>
-            )}
+            {/* Issues */}
+            <div>
+              <H1>Issues</H1>
+              <Subtitle>Your issues that are currently in TODO or IN PROGRESS in the Jira Board</Subtitle>
+              <SizedBox height={10} />
+              {props.loadingIssues ? (
+                <Regular>Loading...</Regular>
+              ) : (
+                <Issues>
+                  {props.issues.map(issue => (
+                    <JIssueItem
+                      issueKey={issue.key}
+                      summary={issue.fields.summary}
+                      onCopy={() => copyToClipboard(getIssueLink(issue.key))}
+                      onMark={() => {}}
+                    />
+                  ))}
+                </Issues>
+              )}
+            </div>
+            {/* Completed issues */}
+            <div>
+              <H1>Incomplete Issues</H1>
+              <Subtitle>Issues that are assigned to you that you can work on</Subtitle>
+            </div>
+            {/* Completed issues */}
+            <div>
+              <H1>Completed Issues</H1>
+              <Subtitle>Issues that you've completed, but not necessarily READY FOR QA</Subtitle>
+              <SizedBox height={10} />
+              {props.loadingCompletedIssues ? (
+                <Regular>Loading...</Regular>
+              ) : (
+                <IssueGroups>
+                  {props.completedIssueGroups.map(issueGroup => (
+                    <div key={issueGroup.dateCompletedString}>
+                      <Regular weight="bold">{issueGroup.dateCompletedString}</Regular>
+                      <SizedBox height={4} />
+                      <Issues>
+                        {issueGroup.issues.map(issue => (
+                          <JIssueItem
+                            issueKey={issue.key}
+                            summary={issue.summary}
+                            onCopy={() => copyToClipboard(getIssueLink(issue.key))}
+                            onMark={() => {}}
+                          />
+                        ))}
+                      </Issues>
+                    </div>
+                  ))}
+                </IssueGroups>
+              )}
+            </div>
           </Body>
         </Main>
       </InnerContainer>
@@ -114,6 +125,9 @@ const Main = styled.div`
 
 const Body = styled.div`
   padding-left: 1.5rem;
+  & > :not(:last-child) {
+    margin-bottom: 28px;
+  }
 `;
 
 const Flex = styled.div`
